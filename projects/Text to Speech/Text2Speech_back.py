5b3e3a35b3d4d22d85763dd38b6efa34e0da5efe
M-Shaffan-Ahmad <mshaffanahmad@gmail.com>
2022-08-23 08:09:18 -0700

    done dona don don

---
+++ projects/Text to Speech/Text2Speech_back.py
@@ -0,0 +1,52 @@
+import pyttsx3
+from tkinter import *
+
+engine = pyttsx3.init()
+
+def save(filename,text):
+    engine.save_to_file(text, '{}.mp3'.format(filename))
+    play()
+
+
+
+def volume_d():
+    engine.setProperty("volume", engine.getProperty("volume")-0.1)
+    play()
+def volume_u():
+    engine.setProperty("volume", engine.getProperty("volume")+0.1)
+    play()
+def rate_d():
+    engine.setProperty("rate", engine.getProperty("rate")-50)
+    print(engine.getProperty("rate"))
+    play()
+def rate_u():
+    engine.setProperty("rate", engine.getProperty("rate")+50)
+    print(engine.getProperty("rate"))
+    play()
+
+def rate(rate=200):
+    engine.setProperty('rate', rate)
+
+def voice_m():
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[0].id)
+    play()
+
+def voice_f():
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[1].id)
+    play()
+
+def play():
+    engine.runAndWait()
+
+def dis(text="Please enter something"):
+    engine.say(text)
+    play()
+
+
+
+
+
+
+
